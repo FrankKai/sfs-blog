d620dfcda130f39a21eb217a98b625dc3667bdbd
frankkai <gaokai20100801@qq.com>
2017-10-20 18:24:06 +0800

    动态存储数据测试

--- server/koa-midware.js
+++ server/koa-midware.js
@@ -17,70 +17,69 @@ var obj2 = {}
 const fs = require("fs")
 
 //读取每个目录下的文章
-fs.readdir('../src/article/categories',(err,files)=>{
-    if(err){
-        throw err
-    }else{
-        files.forEach((v,i,arr)=>{
-            // console.log(v)
-            let cat = '../src/article/categories/'+ v
-            // console.log(cat)
-            fs.readdir(cat,(err,files)=>{
-                files.forEach((v,i,arr)=>{
-                    let smd = cat + "/" + v
-                    fs.readFile(smd,'utf-8',(err,data)=>{
-                        if(err){
-                            throw err
-                        }else{
-                            obj.title = v.slice(0,-3)
-                            obj.content = data
-                            let stat = fs.statSync(smd)
-                            obj.birthtime = stat.birthtime 
-                            db.collection('backendmds').updateMany({title:obj.title},{$set:{content:obj.content}})
-                            db.collection('frontendmds').updateMany({title:obj.title},{$set:{content:obj.content}})
-                            db.collection("backendmds").find({title:obj.title}).toArray(function(err, docs) {
-                                console.dir(docs);
-                                if(docs.length>0){
-                                    obj2 = docs[0]
-                                    mddataarr[0].push(obj2)
-                                    obj2={}
-                                }
-                            })
-                            db.collection("frontendmds").find({title:obj.title}).toArray(function(err, docs) {
-                                console.dir(docs)
-                                if(docs.length>0){
-                                    obj2 = docs[0]
-                                    mddataarr[0].push(obj2)
-                                    obj2={}
-                                }
-                            })
-                        }
+    fs.readdir('../src/article/categories',(err,files)=>{
+        if(err){
+            throw err
+        }else{
+            files.forEach((v,i,arr)=>{
+                // console.log(v)
+                let cat = '../src/article/categories/'+ v
+                // console.log(cat)
+                fs.readdir(cat,(err,files)=>{
+                    files.forEach((v,i,arr)=>{
+                        let smd = cat + "/" + v
+                        fs.readFile(smd,'utf-8',(err,data)=>{
+                            if(err){
+                                throw err
+                            }else{
+                                obj.title = v.slice(0,-3)
+                                obj.content = data
+                                let stat = fs.statSync(smd)
+                                obj.birthtime = stat.birthtime 
+                                db.collection('backendmds').updateMany({title:obj.title},{$set:{content:obj.content}})
+                                db.collection('frontendmds').updateMany({title:obj.title},{$set:{content:obj.content}})
+                                db.collection("backendmds").find({title:obj.title}).toArray(function(err, docs) {
+                                    console.dir(docs);
+                                    if(docs.length>0){
+                                        obj2 = docs[0]
+                                        mddataarr[0].push(obj2)
+                                        obj2={}
+                                    }
+                                })
+                                db.collection("frontendmds").find({title:obj.title}).toArray(function(err, docs) {
+                                    console.dir(docs)
+                                    if(docs.length>0){
+                                        obj2 = docs[0]
+                                        mddataarr[0].push(obj2)
+                                        obj2={}
+                                    }
+                                })
+                            }
+                        })
                     })
                 })
             })
-        })
-    }
-})
+        }
+    })
 //统计目录文件数目
-fs.readdir('../src/article/categories',(err,files)=>{
-    if(err){
-        throw err
-    }else{
-        console.log(files)
-        files.forEach((v,i,arr)=>{
-            fs.readdir('../src/article/categories/'+v,(err,files)=>{
-                console.log(files)
-                categories.name = v
-                categories.num = files.length
-                categories.articles = files
-                mddataarr[1].push(categories)
-                db.collection('category').insertOne(categories);
-                categories = {}
+    fs.readdir('../src/article/categories',(err,files)=>{
+        if(err){
+            throw err
+        }else{
+            console.log(files)
+            files.forEach((v,i,arr)=>{
+                fs.readdir('../src/article/categories/'+v,(err,files)=>{
+                    console.log(files)
+                    categories.name = v
+                    categories.num = files.length
+                    categories.articles = files
+                    mddataarr[1].push(categories)
+                    db.collection('category').insertOne(categories);
+                    categories = {}
+                })
             })
-        })
-    }
-})
-
+        }
+    })
 // app.use(function *(){
 //     this.body = mddataarr;
 // });
@@ -105,12 +104,19 @@ const route = require('koa-route');
 */
 /*post提交评论*/
 const comment = ctx => {
-    ctx.response.body = 'success';
+    var result;
     data = ctx.request.body;
     /*存数据到数据库*/
     // db.collection('test').insertOne(data);
     // db.collection('backendmds').findOne({name:obj.title})
     db.collection(data.category+"mds").update({name:data.mdname},{$push:{comments:data.content}})
+    db.collection(data.category+"mds").findOne({name:data.mdname},(err,doc)=>{
+         console.log(doc['comments'])
+    })
+    // console.log(result)
+    // ctx.response.body = result
+    ctx.response.body = "评论添加成功"
+    // console.log(db.collection(data.category+"mds").findOne({name:data.mdname}))
   };
 app.use(route.post('/comment', comment))
 /*get请求博客*/
