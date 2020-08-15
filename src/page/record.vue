9b2c68a078f8a22198130f90a86a4f8e8dd7ec84
FrankKai <gaokai20100801@gmail.com>
2020-08-15 18:00:49 +0800

    更新项目标题

--- src/page/record.vue
+++ src/page/record.vue
@@ -1,58 +1,61 @@
 <template>
   <div class="record">
-    <!-- {{msg}} -->
     <div class="sidebar">
       <blogger></blogger>
       <timestamp></timestamp>
     </div>
     <navigation></navigation>
     <ul>
-      <li v-for="(item,index) in articles" :key="item.name" @click="filteArticle(index,item.title)">
-        {{item.title}}
-        {{item.birthtime}}
+      <li
+        v-for="(item, index) in articles"
+        :key="item.name"
+        @click="filteArticle(index, item.title)"
+      >
+        {{ item.title }}
+        {{ item.birthtime }}
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-import navigation from '../components/common/navigation.vue'
-import blogger from '../components/blog/blogger.vue'
-import timestamp from '../components/blog/timestamp.vue'
-import { FILTE_ARTICLE } from '../store/mutation-types'
+import navigation from "../components/common/navigation.vue";
+import blogger from "../components/blog/blogger.vue";
+import timestamp from "../components/blog/timestamp.vue";
+import { FILTE_ARTICLE } from "../store/mutation-types";
 
 export default {
-  name: 'record',
-  template: '<record/>',
-  components:{navigation,blogger,timestamp},
-  data () {
+  name: "record",
+  template: "<record/>",
+  components: { navigation, blogger, timestamp },
+  data() {
     return {
-      msg: 'record'
+      msg: "record"
+    };
+  },
+  computed: {
+    articles() {
+      return this.$store.state.data.blog;
     }
   },
-  computed:{
-    articles(){
-      return this.$store.state.data.blog
-    }
-  },
-  methods:{
-    filteArticle: function(index,title){
-      this.$store.commit(FILTE_ARTICLE,index);
-      this.$router.push({path:'/record/'+title});
+  methods: {
+    filteArticle: function(index, title) {
+      this.$store.commit(FILTE_ARTICLE, index);
+      this.$router.push({ path: "/record/" + title });
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-.record{
-  width:80%;
-    ul{
+.record {
+  width: 80%;
+  ul {
     padding: 0;
     margin: 0;
     width: 70%;
-    li{
+    li {
       list-style: none;
       display: block;
       margin-top: 5px;
@@ -61,15 +64,16 @@ export default {
       padding: 5px;
       background: #e4e8f1;
       border-radius: 5px;
-      &:hover{
+      cursor: pointer;
+      &:hover {
         background: #8391a5;
         color: #fff;
       }
     }
-  };
+  }
 }
-.sidebar{
-  float:right;
+.sidebar {
+  float: right;
   width: 30%;
 }
 </style>
